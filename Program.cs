151d9c29d5fb84fcdc67188b80b732dbcb78b007
mateuzin <pg9779628@gmail.com>
2021-06-24 22:50:03 -0300

    ...

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace gerador_de_senha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("password size: ");
+            int size = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            Random random = new Random();
+            int maxPassword = size;
+            string allCaracters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            string password = "";
+            for(int i = 1;i <= maxPassword; i++)
+            {
+                int numberRandom = random.Next(0, allCaracters.Length);
+                char caracterRandom = allCaracters[numberRandom];
+                Console.WriteLine(caracterRandom);
+                Console.Clear();
+                password += caracterRandom;                
+            }
+            ProcessStartInfo ps = new ProcessStartInfo();
+            ps.FileName = "cmd.exe";
+            ps.WindowStyle = ProcessWindowStyle.Normal;
+            ps.Arguments = "/c msg * Senha Gerada: " + password;
+            Process.Start(ps);
+        }
+    }
+}
